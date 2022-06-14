3cd8c55283a88c2e7d35046c91e6bdc35f5b81ce
Sabrina <sabrina.samuel@madetech.com>
2022-06-14 14:40:58 +0100

    added in win conditions

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -10,4 +10,40 @@ describe 'how to play tennis' do
     it "returns two wins for player one and two losses for player two" do
         expect(tennis_winner('player_1 player_1')).to eq("2, 0")
     end
+    it "returns four wins for player one and one win for player two" do
+        expect(tennis_winner("player_1 player_2 player_1 player_1 player_1")).to eq("4, 1")
+    end
+end
+
+describe 'how to score tennis' do
+    it "wins when player 1 scores 4 points" do
+        expect(match_outcome(4, 0)).to eq("Player 1 wins")
+    end
+    it "wins when player 2 scores 4 points" do
+        expect(match_outcome(0, 4)).to eq("Player 2 wins")
+    end
+    it "wins when player 1 scores 5 points and player 2 scores 3 points" do
+        expect(match_outcome(5, 3)).to eq("Player 1 wins")
+    end
+    it "wins when player 1 scores 4 points and player 2 scores 6 points" do
+        expect(match_outcome(4, 6)).to eq("Player 2 wins")
+    end
+    it "returns deuce when both players score 3" do
+        expect(match_outcome(3, 3)).to eq("Deuce")
+    end
+    it "returns deuce when both players score 6" do
+        expect(match_outcome(6, 6)).to eq("Deuce")
+    end
+    it "returns advantage when player 1 has 4 and player 2 has 3 points" do
+        expect(match_outcome(4, 3)).to eq("Advantage player 1")
+    end
+    it "returns advantage when player 1 has 6 and player 2 has 5 points" do
+        expect(match_outcome(6, 5)).to eq("Advantage player 1")
+    end
+    it "returns advantage when player 2 has 4 and player 1 has 3 points" do
+        expect(match_outcome(3, 4)).to eq("Advantage player 2")
+    end
+    it "returns advantage when player 2 has 7 and player 1 has 6 points" do
+        expect(match_outcome(6, 7)).to eq("Advantage player 2")
+    end
 end
--- tennis.rb
+++ tennis.rb
@@ -5,10 +5,31 @@
 # player2 "loss"
 
 def tennis_winner(player_name)
-    
-    if player_name == "player_1"
-        "1, 0"
+    p1_score = 0
+    p2_score = 0
+    winners = player_name.split
+    winners.each do |winner|
+    if winner == "player_1"
+        p1_score += 1
     else
-        "0, 1"
+        p2_score += 1
+    end
+    end
+    return("#{p1_score}, #{p2_score}")
+end
+
+def match_outcome(p1_score, p2_score)
+    if p1_score >= 4 && p1_score != p2_score && p1_score - p2_score > 1
+        return "Player 1 wins"
+    elsif p2_score >= 4 && p2_score != p1_score && p2_score - p1_score > 1
+        return "Player 2 wins"
+    elsif p1_score >= 3 && p1_score == p2_score
+        return "Deuce"
+    elsif p1_score > 3 && p1_score - p2_score == 1
+        return "Advantage player 1"
+    elsif p2_score > 3 && p2_score - p1_score == 1
+        return "Advantage player 2"
+    else
+        return ""
     end
 end

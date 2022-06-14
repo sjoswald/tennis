b25d4f5db0738801dd1de2dd7c432ae93bd48670
Sabrina <sabrina.samuel@madetech.com>
2022-06-14 11:40:47 +0100

    restarted tennis files

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,23 +1,7 @@
 require './tennis.rb'
 
 describe 'how to play tennis' do
-    it 'should return 1 when player 1 win' do
-        expect(player1('win')).to eq(1)
+    it "adds win to player 1" do
+        expect(tennis_winner("player 1")).to eq(1)
     end
-    it "should return 2 when player 1 win win" do
-        expect(player1("win win")).to eq(2)
-    end
-    it "should return 1 when player 1 win loss" do
-        expect(player1("win loss")).to eq(1)
-    end
-    it "should return 2 when player 1 win loss win" do
-        expect(player1("win loss win")).to eq(2)
-    end
-    it 'should return 1 when player 2 win' do
-        expect(player2('win')).to eq(1)
-    end
-    it "should return the score for two players" do
-        expect(player1("win")).to eq(1)
-        expect(player2("loss")).to eq(0)
-    end
-end -
+end
--- tennis.rb
+++ tennis.rb
@@ -1,7 +1,9 @@
-def player1(win)
-    win.split.count("win")
-end
+# what is actually sent is who wins
+# tennis(player_name)
+#  when player 1 wins, increment player 1 win count, oherwise increment player 2 win count
+# player1 "win"
+# player2 "loss"
 
-def player2(win)
-    win.split.count("win")
+def tennis_winner(player_name)
+    1
 end
